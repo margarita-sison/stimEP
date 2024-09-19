bbfa5b9fa8c4f3ebe4f170a3293eaa6a466d57d6
Margarita Sison <margaritasiysison@gmail.com>
2024-09-18 20:16:20 -0400

    Added to 'getepochs' function

--- functions/ms_getepochs.m
+++ functions/ms_getepochs.m
@@ -15,13 +15,33 @@ function [epoch_tensor, ppt] = ms_getepochs(signal2epoch, timewindow, fs, makepp
 % epoch_tensor (3-dim double)   - 
 % ppt
 
-% Convert signal to time domain
+n_chans = size(signal2epoch,1);
 
-
-
+samples_per_ms = fs/1000;
 
 start_time = timewindow(1);
+pre_peak_len = start_time*samples_per_ms;
+
 end_time = timewindow(2);
+post_peak_len = end_time*samples_per_ms;
 
+epoch_tensor = zeros(n_chans, length(pk_locs), length(pre_peak_len:post_peak_len));
 
+for c = 1:length(n_chans)
+    chan = signal2epoch(c,:);
+
+    for p = 1:length(pk_locs)
+        pk_loc = pk_locs(p);
+        
+        if 
+            continue
+        end
+        epoch = chan(pk_loc+pre_peak_len:pk_loc+post_peak_len);
+
+        epoch_tensor(c,p,:) = epoch;
+    end
+end
+
+if makeppt == 1
+    %yaxis = -20:1/22:200
 end
