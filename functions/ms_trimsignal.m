2973a4063ccfa24948d8be2e13909166c70217a6
Margarita Sison <margaritasiysison@gmail.com>
2024-09-19 14:47:30 -0400

    Added to 'getepochs'

--- functions/ms_trimsignal.m
+++ functions/ms_trimsignal.m
@@ -6,8 +6,8 @@ fig.WindowState = 'maximized';
 plot(signal2trim)
 [x, y] = ginput(2);
 
-start_idx = x(1);
-end_idx = x(2);
+start_idx = round(x(1));
+end_idx = round(x(2));
 segment = signal2trim(start_idx:end_idx);
 plot(segment)
 end
--- stimEP_pipeline.m
+++ stimEP_pipeline.m
@@ -1,17 +1,17 @@
-addpath("/Users/margaritasison/GitHub/stimEP/functions/")
-datadir = "/Users/margaritasison/Downloads/ORdata_copes_MS/";
-ephys = "ephys015/";
-% addpath("C:\Users\Miocinovic_lab\Documents\mssison\GitHub\stimEP\functions\")
-% 
-% datadir = "C:\Users\Miocinovic_lab\Documents\mssison\ORdata_copies_MS\";
-% ephys = "ephys015\";
+% addpath("/Users/margaritasison/GitHub/stimEP/functions/")
+% datadir = "/Users/margaritasison/Downloads/ORdata_copes_MS/";
+% ephys = "ephys015/";
+
+addpath("C:\Users\Miocinovic_lab\Documents\mssison\GitHub\stimEP\functions\")
+datadir = "C:\Users\Miocinovic_lab\Documents\mssison\ORdata_copies_MS\";
+ephys = "ephys015\";
 signaltype = "emg";
 
 ms_plotsignals(datadir, ephys, signaltype)
 %%
 signal2trim = emg(3,:);
-%[start_idx, end_idx, segment] = ms_trimsignal(signal2trim);
-[~, ~, segment] = ms_trimsignal(signal2trim);
+[start_idx, end_idx, segment] = ms_trimsignal(signal2trim);
+%[~, ~, segment] = ms_trimsignal(signal2trim);
 
 %%
 [peaks, pk_locs, pk_widths, pk_proms, min_prom] = ms_findpeaks(segment);
